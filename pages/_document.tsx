14aa34a3e879e15b91c40aafeb7f395515525a5e
Yog <yog.panjarale@gmail.com>
2021-07-20 22:47:37 +0530

    changed image and seo tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,10 @@
 import Document, { Head, Main, NextScript } from 'next/document'
 
+const {title,description,image}={
+  title:`BlueLearn SPRINT Let's change the world together with Open source!`,
+  description:`BlueLearn Open-Sauce-Club is open to everyone whether you're new to development, a student or a long-time contributor. Open your first pull request and generate a personalized music certificate at here`,
+  image:`/OpenSauce_Poster_final.png`
+}
 
 export default class MyDocument extends Document {
   render() {
@@ -8,26 +13,23 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="shortcut icon" href="/static/favicon.ico" />
           {/* <!-- Primary Meta Tags --> */}
-<title>BlueLearn SPRINT Let's change the world together with Open source!</title>
-<meta name="title" content="BlueLearn SPRINT Let's change the world together with Open source!"/>
-<meta name="description" content="This repo is to welcome beginners to Github and the opensource community by helping them learn how to make their first PR and contributions to open source!
-So, try contributing in a meaningful way to other BlueLearn SPRINT issues and if you still want to generate a music certificate or add yourself to bluesprint.vercel.app go ahead and raise a PR."/>
+<title>{title}</title>
+<meta name="title" content={title}/>
+<meta name="description" content={description}/>
 
 {/* <!-- Open Graph / Facebook --> */}
 <meta property="og:type" content="website"/>
 <meta property="og:url" content="https://bluesprint.vercel.app/"/>
-<meta property="og:title" content="BlueLearn SPRINT Let's change the world together with Open source!"/>
-<meta property="og:description" content="This repo is to welcome beginners to Github and the opensource community by helping them learn how to make their first PR and contributions to open source!
-So, try contributing in a meaningful way to other BlueLearn SPRINT issues and if you still want to generate a music certificate or add yourself to bluesprint.vercel.app go ahead and raise a PR."/>
-<meta property="og:image" content="/ss.jpg"/>
+<meta property="og:title" content={title}/>
+<meta property="og:description" content={description}/>
+<meta property="og:image" content={image}/>
 
 {/* <!-- Twitter --> */}
 <meta property="twitter:card" content="summary_large_image"/>
 <meta property="twitter:url" content="https://bluesprint.vercel.app/"/>
-<meta property="twitter:title" content="BlueLearn SPRINT Let's change the world together with Open source!"/>
-<meta property="twitter:description" content="This repo is to welcome beginners to Github and the opensource community by helping them learn how to make their first PR and contributions to open source!
-So, try contributing in a meaningful way to other BlueLearn SPRINT issues and if you still want to generate a music certificate or add yourself to bluesprint.vercel.app go ahead and raise a PR."/>
-<meta property="twitter:image" content="/ss.jpg"/>
+<meta property="twitter:title" content={title}/>
+<meta property="twitter:description" content={description}/>
+<meta property="twitter:image" content={image}/>
         </Head>
         <body>
           <Main />
